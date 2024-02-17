e9ff2bf34f7eebc3cc902ea6cac214bdf2636ff5
KeTr <kevintremmel@gmx.de>
2024-02-17 18:35:56 +0100

    Window Dragging

--- DragAnchor.cs
+++ DragAnchor.cs
@@ -1,5 +1,43 @@
+using System;
 using Godot;
 
 namespace TonstudioDiscoball;
 
-public partial class DragAnchor : TextureRect { }
+public partial class DragAnchor : TextureRect {
+
+    private bool _hasMouseFocus;
+    private bool _dragging;
+    private Vector2 _dragPoint;
+
+    public override void _Ready() {
+        MouseEntered += () => {
+            if (Visible) {
+                _hasMouseFocus = true;
+            }
+        };
+        MouseExited += () => { _hasMouseFocus = false; };
+    }
+
+    public override void _Input(InputEvent ev) {
+        if (ev.IsActionPressed("LeftClick") && _hasMouseFocus) {
+            _dragPoint = GetGlobalMousePosition();
+            _dragging = true;
+        }
+        else if (ev.IsActionReleased("LeftClick") && _hasMouseFocus)
+            _dragging = false;
+        else if (_dragging && ev is InputEventMouseMotion) {
+            var positionChange = GetGlobalMousePosition() - _dragPoint;
+            var newPosition = GetViewport().GetWindow().Position +
+                              new Vector2I((int)positionChange.X, (int)positionChange.Y);
+            DisplayServer.WindowSetPosition(newPosition);
+        }
+    }
+
+    public override void _Notification(int notificationType) {
+        if (notificationType != NotificationDragEnd) return;
+
+        Console.WriteLine("drag end");
+        _dragging = false;
+        _dragPoint = default;
+    }
+}
